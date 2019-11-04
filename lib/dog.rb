4700b193476b2abbc79d41200a7d6eaa947b2153
Justin Miller <jayman1294@gmail.com>
2019-11-04 04:39:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Add your code here
+class Dog 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+  @@all << self  
+  
+  end 
+    
+  def self.all
+  @@all
+  end 
+  
+  def self.print_all
+  
+  end
+end
